894d792f28e838bca953d93e860a4095a404d2a0
Zach Leslie <xaque208@gmail.com>
2017-01-26 17:09:46 -0800

    modulesync 2017-01-26

--- spec/defines/group_spec.rb
+++ spec/defines/group_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 describe 'account::group' do
-
   context 'with values for all required parameters' do
     let(:title) { 'humans' }
-    let(:params) {{
-      :members => [],
-      :gid => 2000,
-    }}
+    let(:params) do
+      {
+        members: [],
+        gid: 2000
+      }
+    end
 
-    it { should contain_group('humans').with_gid(2000) }
-    it { should contain_groupmembership('humans').with_members([]) }
+    it { is_expected.to contain_group('humans').with_gid(2000) }
+    it { is_expected.to contain_groupmembership('humans').with_members([]) }
   end
 end
-
